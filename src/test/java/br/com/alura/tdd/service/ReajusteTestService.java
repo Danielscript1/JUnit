fd54ae83f587b877ee43abdc390a2438bb4ea5e9
Danielscript1 <danielsousagit@gmail.com>
2022-01-05 20:43:20 -0300

    Organizando os codigos de teste -> com algumas anotaçoes

--- src/test/java/br/com/alura/tdd/service/ReajusteTestService.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteTestService.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
@@ -12,30 +14,41 @@ import br.com.alura.tdd.modelo.Reajuste;
 import br.com.alura.tdd.modelo.enums.Desempenho;
 
 class ReajusteTestService {
+	
+	private Reajuste service;
+	private Funcionario funcionario;
+	@BeforeEach //anotação JUnit que faz a chamada do metodo antes
+	public void Inicializar() {
+		this.funcionario = new Funcionario("jakcson",LocalDate.now(),new BigDecimal("1000.00"));
+		this.service = new Reajuste();
+	}
+
 	//teste TDD
 	@Test
 	public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
-		Reajuste service = new Reajuste();
-		Funcionario funcionario = new Funcionario("jakcson",LocalDate.now(),new BigDecimal("1000.00"));
+		
 		service.concederReajuste(funcionario,Desempenho.A_DESEJAR);
 		assertEquals(new BigDecimal("1030.00"),funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
-		Reajuste service = new Reajuste();
-		Funcionario funcionario = new Funcionario("jakcson",LocalDate.now(),new BigDecimal("1000.00"));
+		
 		service.concederReajuste(funcionario,Desempenho.BOM);
 		assertEquals(new BigDecimal("1150.00"),funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
-		Reajuste service = new Reajuste();
-		Funcionario funcionario = new Funcionario("jakcson",LocalDate.now(),new BigDecimal("1000.00"));
+		
 		service.concederReajuste(funcionario,Desempenho.OTIMO);
 		assertEquals(new BigDecimal("1200.00"),funcionario.getSalario());
 	}
 	
+	/* --algumas anotaçoes----
+	 * @AfterEach -> rodar apos a execução dos teste
+	 * @BeforeAll -> roda apenas uma vez antes de todos os metodos, esse metodo tem que ser static
+	 * @AfterAll -> roda depois, obs esse metodo que roda apenas uma vez, tem que ser static
+	 * */
 
 }
